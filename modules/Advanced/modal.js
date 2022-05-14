2aa36cad4e626d026ed36fd406e5711173e4e75d
Vaz-Tiago <tvaz@redventures.com>
2022-05-14 19:51:18 -0300

    add logic to open

--- modules/Advanced/modal.js
+++ modules/Advanced/modal.js
@@ -2,6 +2,7 @@ class Modal extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
+    this.isOpen = false;
     this.shadowRoot.innerHTML = `
       <style>
         #backdrop {
@@ -11,11 +12,83 @@ class Modal extends HTMLElement {
           width: 100%;
           height: 100vh;
           background: rgba(0,0,0,0.75);
+          
+          opacity: 0;
+          pointer-events: none;
+          z-index: 10;
+        }
+
+        #modal {
+          position: fixed;
+          top: 15vh;
+          left: 25%;
+          width: 50%;
+          height: 30rem;
+          background-color: white;
+          border-radius: 3px;
+          box-shadow: 0 2px 8px rgba(0,0,0,0.26);
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+
+          opacity: 0;
+          pointer-events: none;
+          z-index: 100;
+        }
+
+        :host([opened]) #backdrop,
+        :host([opened]) #modal {
+          opacity: 1;
+          pointer-events: 'all';
+        }
+
+        header {
+          padding: 1rem;
+        }
+
+        header h1 {
+          font-size: 1.25rem;
+        }
+
+        #main {
+          padding: 1rem;
+        }
+
+        #actions {
+          border-top: 1px solid #ccc;
+          padding: 1rem;
+          display: flex;
+          justify-content: flex-end;
+          gap: 0.5rem;
         }
       </style>
       <div id='backdrop'></div>
-      <div id='modal'></div>
+      <div id='modal'>
+        <header>
+          <h1>Please Confirm</h1>
+        </header>
+        <section id='main'>
+          <slot></slot>
+        </section>
+        <section id='actions'>
+          <button>Cancel</button>
+          <button>Confirm</button>
+        </section>
+      </div>
     `;
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (this.hasAttribute("opened")) this.isOpen = true;
+    else this.isOpen = false;
+  }
+
+  static get observedAttributes() {
+    return ["opened"];
+  }
+
+  open() {
+    this.setAttribute("opened", "");
   }
 }
 
