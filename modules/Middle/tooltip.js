2ad3ce1cbca3061589e00b25026c78d82d8bd668
Vaz-Tiago <tvaz@redventures.com>
2022-05-14 18:29:50 -0300

    component state observable and cleanup function

--- modules/Middle/tooltip.js
+++ modules/Middle/tooltip.js
@@ -2,6 +2,7 @@ class ToolTip extends HTMLElement {
   constructor() {
     super();
     this._tooltipContainer;
+    this._tooltipIcon;
     this._tooltipText = "Default tooltip value!";
     this.attachShadow({ mode: "open" });
 
@@ -58,10 +59,31 @@ class ToolTip extends HTMLElement {
     if (this.hasAttribute("text")) {
       this._tooltipText = this.getAttribute("text");
     }
-    const tooltipIcon = this.shadowRoot.querySelector("span");
-    tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
-    tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
-    this.shadowRoot.appendChild(tooltipIcon);
+    this._tooltipIcon = this.shadowRoot.querySelector("span");
+    this._tooltipIcon.addEventListener(
+      "mouseenter",
+      this._showTooltip.bind(this)
+    );
+    this._tooltipIcon.addEventListener(
+      "mouseleave",
+      this._hideTooltip.bind(this)
+    );
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) return;
+
+    // necessary, because we can observable more then one attribute
+    if (name === "text") this._tooltipText = newValue;
+  }
+
+  static get observedAttributes() {
+    return ["text"];
+  }
+
+  disconnectedCallback() {
+    this._tooltipIcon.removeEventListener("mouseenter", this._showTooltip);
+    this._tooltipIcon.removeEventListener("mouseleave", this._hideTooltip);
   }
 
   _showTooltip() {
